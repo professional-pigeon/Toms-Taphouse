3055f44cb9bb54e0bff71b3ace20f1dd6c921bdd
professional-pigeon <kyle.kayperez@gmail.com>
2021-09-10 16:28:02 -0700

    center photo

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,11 +4,15 @@ import Bar from './bar.jpg'
 
 function Photo() {
   const photoStyles = {
-    maxWidth: '100%'
+    maxWidth: '100%',
+    alignItems: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    display: "block"
   }
   return (
     <React.Fragment>
-      <img src={Bar} alt="Otter holding a beer" style={photoStyles} />
+      <img src={Bar} alt="Neon bar sign" style={photoStyles} />
     </React.Fragment>
   );
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,19 @@
 import React from "react";
 import About from "./About";
 import Photo from "./Photo"
+import { Card } from "react-bootstrap"
 
 function Sidebar() {
     return (
     <React.Fragment>
-      <Photo />
-      <About />
+      <Card>
+        <Card.Title>
+          <Photo />
+        </Card.Title>
+        <Card.Text>
+          <About />
+        </Card.Text>
+      </Card>
     </React.Fragment>
   )
 }
